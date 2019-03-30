ee3ab331980e690a08ac40c225ddecc07dfc7943
Smilte <smilte.petronyte@gmail.com>
2019-03-30 21:08:37 +0000

    Create alexa_lambda.py

---
+++ alexa_lambda.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+
+# This is a Color Picker Alexa Skill.
+# The skill serves as a simple sample on how to use  
+# session attributes.
+
+import logging
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model import Response
+from ask_sdk_model.ui import SimpleCard
+from my_lyrics import *
+
+
+skill_name = "Alexa lyrics"
+
+help_text = ("Please tell me the name of the song.")
+# song_slot_key = "SONG"
+# song_slot = "Song"
+
+l, a, s = get_popular_song()
+index = 0
+
+sb = SkillBuilder()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def get_two_lines(l) : 
+    
+    # if(index + 1 >= len(l) ) : 
+
+    lines = l[index] + l[index+1]
+    index += 2
+    return lines
+
+@sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
+def launch_request_handler(handler_input):
+    """Handler for Skill Launch."""
+    # type: (HandlerInput) -> Response
+    speech = "Welcome to the Alexa Lyrics. Here is the first line of the song:"
+    lines = get_two_lines(l)
+
+    handler_input.response_builder.speak(speech + " "+ lines +" "+ help_text).ask(help_text)
+    return handler_input.response_builder.response
+
+@sb.request_handler(can_handle_func=is_intent_name("QuizIntent"))
+def quiz_intent_handler(handler_input):
+    # type: (HandlerInput) -> Response
+    speech = "Welcome to the Alexa Lyrics. Here is the first line of the song:"
+    lines = get_two_lines(l)
+
+    handler_input.response_builder.speak(speech + " "+ lines +" "+ help_text).ask(help_text)
+    return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
+def help_intent_handler(handler_input):
+    """Handler for Help Intent."""
+    # type: (HandlerInput) -> Response
+    handler_input.response_builder.speak(help_text).ask(help_text)
+    return handler_input.response_builder.response
+
+
+@sb.request_handler(
+    can_handle_func=lambda handler_input:
+        is_intent_name("AMAZON.CancelIntent")(handler_input) or
+        is_intent_name("AMAZON.StopIntent")(handler_input))
+def cancel_and_stop_intent_handler(handler_input):
+    """Single handler for Cancel and Stop Intent."""
+    # type: (HandlerInput) -> Response
+    speech_text = "Goodbye!"
+
+    return handler_input.response_builder.speak(speech_text).response
+
+
+@sb.request_handler(can_handle_func=is_request_type("SessionEndedRequest"))
+def session_ended_request_handler(handler_input):
+    """Handler for Session End."""
+    # type: (HandlerInput) -> Response
+    return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=is_intent_name("AnswerIntent"))
+def answer_intent_handler(handler_input):
+    # type: (HandlerInput) -> Response
+    guess_song = handler_input.request_envelope.request.intent.slots["Song"]
+    guess_musician = handler_input.request_envelope.request.intent.slots["Musician"]
+
+    if guess_song == s :
+        speech = "That is correct. The name of the song is {}. Goodbye!!".format(s)
+        handler_input.response_builder.set_should_end_session(True)
+    else:
+        speech = "Sorry that is incorrect. Would you like to try again?"+" "+ help_text
+        handler_input.response_builder.ask(help_text)
+
+    handler_input.response_builder.speak(speech)
+    return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=is_intent_name("HintIntent"))
+def my_color_handler(handler_input):
+
+    next_line = get_two_lines(l)
+    speech = "The next line is: "+ next_line +help_text
+
+    handler_input.response_builder.speak(speech)
+    return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=is_intent_name("AMAZON.FallbackIntent"))
+def fallback_handler(handler_input):
+    """AMAZON.FallbackIntent is only available in en-US locale.
+    This handler will not be triggered except in that locale,
+    so it is safe to deploy on any locale.
+    """
+    # type: (HandlerInput) -> Response
+    line = get_two_lines(l)
+    speech = ("The {} skill can't help you with that.".format(skill_name))
+    reprompt = ("Would you like to guess a song? Here is the first line:"+line+" "+help_text)
+    handler_input.response_builder.speak(speech).ask(reprompt)
+    return handler_input.response_builder.response
+
+
+def convert_speech_to_text(ssml_speech):
+    """convert ssml speech to text, by removing html tags."""
+    # type: (str) -> str
+    s = SSMLStripper()
+    s.feed(ssml_speech)
+    return s.get_data()
+
+
+@sb.global_response_interceptor()
+def add_card(handler_input, response):
+    """Add a card by translating ssml text to card content."""
+    # type: (HandlerInput, Response) -> None
+    response.card = SimpleCard(
+        title=skill_name,
+        content=convert_speech_to_text(response.output_speech.ssml))
+
+
+@sb.global_response_interceptor()
+def log_response(handler_input, response):
+    """Log response from alexa service."""
+    # type: (HandlerInput, Response) -> None
+    print("Alexa Response: {}\n".format(response))
+
+
+@sb.global_request_interceptor()
+def log_request(handler_input):
+    """Log request to alexa service."""
+    # type: (HandlerInput) -> None
+    print("Alexa Request: {}\n".format(handler_input.request_envelope.request))
+
+
+@sb.exception_handler(can_handle_func=lambda i, e: True)
+def all_exception_handler(handler_input, exception):
+    """Catch all exception handler, log exception and
+    respond with custom message.
+    """
+    # type: (HandlerInput, Exception) -> None
+    print("Encountered following exception: {}".format(exception))
+
+    speech = "Sorry, there was some problem. Please try again!!"
+    handler_input.response_builder.speak(speech).ask(speech)
+
+    return handler_input.response_builder.response
+
+
+######## Convert SSML to Card text ############
+# This is for automatic conversion of ssml to text content on simple card
+# You can create your own simple cards for each response, if this is not
+# what you want to use.
+
+from six import PY2
+try:
+    from HTMLParser import HTMLParser
+except ImportError:
+    from html.parser import HTMLParser
+
+
+class SSMLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.full_str_list = []
+        if not PY2:
+            self.strict = False
+            self.convert_charrefs = True
+
+    def handle_data(self, d):
+        self.full_str_list.append(d)
+
+    def get_data(self):
+        return ''.join(self.full_str_list)
+
+################################################
+
+
+# Handler to be provided in lambda console.
+lambda_handler = sb.lambda_handler()
